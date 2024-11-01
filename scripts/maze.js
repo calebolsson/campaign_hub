2939bbe409fb5204ce8b0cfdc67729f1325cf7e4
calebolsson <114210583+calebolsson@users.noreply.github.com>
2024-11-01 18:10:37 -0500

    Update maze.js
    adjust player speeds

--- scripts/maze.js
+++ scripts/maze.js
@@ -1,5 +1,5 @@
 //let WUBRG = [white, blue, black, red, green];
-let map_zone = 4;
+let map_zone = 3;
 let save = { x: 59, y: 23 };//(31,28) is the middle
 const portal_connections = [
     [null, { x: 15, y: 55 }, null, null, { x: 39, y: 1 }],
@@ -55,8 +55,8 @@ let data = {
         angle: 0,
         radius: 20,
         speed: {
-            movement: 0.02,
-            rotation: 0.25
+            movement: 0.05,
+            rotation: 0.75
         }
     },
     map: [
