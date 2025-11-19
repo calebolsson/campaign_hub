c982f94f26b6d08b0fef108da081c87474ce70e3
Caleb Olsson <caleb.l.olsson@gmail.com>
2025-11-19 13:46:07 -0600

    rebuilt encyclopedia display/sorting/grouping to enable new encyclopedia headers

--- Services/EncyclopediaService.cs
+++ Services/EncyclopediaService.cs
@@ -1,6 +1,8 @@
 ﻿using DataAccessLibrary;
 using DataAccessLibrary.Models;
 using Microsoft.AspNetCore.Routing.Template;
+using System.ComponentModel;
+using System.Drawing;
 
 namespace campaign_hub.Services
 {
@@ -10,12 +12,14 @@ namespace campaign_hub.Services
         {
             Campaign,
             Character,
-            Guild
+            Guild,
+            Color
         };
         public List<KeyValuePair<int, Source>> entries { get; set; } = new();
         public List<CampaignModel> campaigns { get; set; } = new();
         public List<CharacterModel> characters { get; set; } = new();
         public List<GuildModel> guilds { get; set; } = new();
+        public List<FilterService.Colors> colors { get; set; } = Enum.GetValues<FilterService.Colors>().ToList();
         public async Task LoadEncyclopedia(ICampaignData _db)
         {
             campaigns = await _db.getCampaigns();
@@ -37,29 +41,122 @@ namespace campaign_hub.Services
             switch (filter.CategoryFilter)
             {
                 case FilterService.Categories.Character:
-                    foreach (int i in indexes(characters.Count(), filter.ascending))
-                    {
-                        if (filter.CampaignFilter == "0000" || characters[i].campaign_id.ToString() == filter.CampaignFilter)
-                            entries.Add(new KeyValuePair<int, Source>(i, Source.Character));
-                    }
+                    SortCharacters(filter);
                     break;
                 case FilterService.Categories.Guild:
-                    foreach (int i in indexes(guilds.Count(), filter.ascending))
-                    {
-                        if (filter.CampaignFilter == "0000" || guilds[i].Campaign_Id.ToString() == filter.CampaignFilter)
-                            entries.Add(new KeyValuePair<int, Source>(i, Source.Guild));
-                    }
+                    SortGuilds(filter);
                     break;
-                    // Additional cases for Location, Event, Other can be added here
+                case FilterService.Categories.Location:
+                    // Implement SortLocation 
+                    break;
+                case FilterService.Categories.Event:
+                    // Implement SortEvent 
+                    break;
+                case FilterService.Categories.Other:
+                    // Implement SortOther 
+                    break;
             }
             // Sorting based on GroupFilter
-            entries = filter.GroupFilter switch
+            switch (filter.GroupFilter)
             {
-                FilterService.Groups.Name => entries.OrderBy(e => e.Value).ToList(),
-                _ => entries
-            };
+                case FilterService.Groups.Name:
+                    entries = entries.OrderBy(GetName).ToList();
+                    break;
+                case FilterService.Groups.Color:
+                    GroupByColor(filter);
+                    break;
+                case FilterService.Groups.Guild:
+                    if (filter.CategoryFilter != FilterService.Categories.Guild)
+                        GroupByGuild(filter);
+                    break;
+                case FilterService.Groups.Class:
+                    // Implement GroupByClass 
+                    break;
+                case FilterService.Groups.Plane:
+                    // Implement GroupByPlane 
+                    break;
+                case FilterService.Groups.Campaign:
+                    // Implement GroupByCampaign 
+                    break;
+            }
             return;
         }
+
+        private void GroupByGuild(FilterService filter)
+        {
+            List<KeyValuePair<int, Source>> temp = new();
+            foreach (var guild in guilds)
+            {
+                KeyValuePair<int, Source> this_selection = new KeyValuePair<int, Source>(guilds.IndexOf(guild), Source.Guild);
+                temp.Add(this_selection);
+                var selected_entries = entries.Where(e => GetTags(e).Contains(ParseTags(guild.Tags)[0])).ToList();
+                temp.AddRange(selected_entries);
+            }
+            entries = temp;
+        }
+
+        private void GroupByColor(FilterService filter)
+        {
+            List<KeyValuePair<int, Source>> temp = new();
+            foreach (var color in colors)
+            {
+                KeyValuePair<int, Source> this_selection = new KeyValuePair<int, Source>(colors.IndexOf(color), Source.Color);
+                temp.Add(this_selection);
+                var selected_entries = entries.Where(e => GetTags(e).Contains(color.ToString())).ToList();
+                temp.AddRange(selected_entries);
+            }
+            entries = temp;
+        }
+
+        private void SortCharacters(FilterService filter)
+        {
+            foreach (int i in indexes(characters.Count(), filter.ascending))
+            {
+                if (filter.CampaignFilter == "0000" || characters[i].campaign_id.ToString() == filter.CampaignFilter)
+                    entries.Add(new KeyValuePair<int, Source>(i, Source.Character));
+            }
+        }
+
+        private void SortGuilds(FilterService filter)
+        {
+            foreach (int i in indexes(guilds.Count(), filter.ascending))
+            {
+                if (filter.CampaignFilter == "0000" || guilds[i].Campaign_Id.ToString() == filter.CampaignFilter)
+                    entries.Add(new KeyValuePair<int, Source>(i, Source.Guild));
+            }
+        }
+
+        private string? GetName(KeyValuePair<int, Source> entry)
+        {
+            return entry.Value switch
+            {
+                EncyclopediaService.Source.Character => characters[entry.Key].first_name,
+                EncyclopediaService.Source.Guild => guilds[entry.Key].Name,
+                _ => "" // base case
+            };
+        }
+        private string? GetTags(KeyValuePair<int, Source> entry)
+        {
+            return entry.Value switch
+            {
+                EncyclopediaService.Source.Character => characters[entry.Key].tags,
+                EncyclopediaService.Source.Guild => guilds[entry.Key].Tags,
+                //EncyclopediaService.Source.Campaign => campaigns[entry.Key].tags,
+                _ => "" // base case
+            };
+        }
+
+        private List<string> ParseTags(string tags)
+        {
+            return tags.TrimEnd().Split(',').ToList();
+        }
+
+        //private string? ExtractColors(string tags)
+        //{
+        //    var parsed = ParseTags(tags);
+        //    return parsed.ToList<string>();
+        //}
+
 
         //public Array indexes(int list_count, bool ascending)
         //{
@@ -67,6 +164,7 @@ namespace campaign_hub.Services
         //    else return Enumerable.Range(0, list_count).Reverse().ToArray();
         //}
 
+        // I think this is faster because it doesn't create/reverse an array
         public IEnumerable<int> indexes(int list_count, bool ascending)
         {
             if (ascending)
